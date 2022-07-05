c53310d66829eb47bdf13510eb50a1e4d48cf168
Sam-2019 <samtmartey@gmail.com>
2022-07-05 07:35:06 +0100

    data import

---
+++ src/utils/data.tsx
@@ -0,0 +1,83 @@
+export const userData = [
+ {
+  key: 1,
+  name: "John Brown",
+  age: 32,
+  address: "New York No. 1 Lake Park",
+  tags: ["nice", "developer"],
+ },
+ {
+  key: 2,
+  name: "Jim Green",
+  age: 42,
+  address: "London No. 1 Lake Park",
+  tags: ["loser"],
+ },
+ {
+  key: 3,
+  name: "Joe Black",
+  age: 32,
+  address: "Sidney No. 1 Lake Park",
+  tags: ["cool", "teacher"],
+ },
+ {
+  key: 4,
+  name: "John Brown",
+  age: 32,
+  address: "New York No. 1 Lake Park",
+  tags: ["nice", "developer"],
+ },
+ {
+  key: 5,
+  name: "Jim Green",
+  age: 42,
+  address: "London No. 1 Lake Park",
+  tags: ["loser"],
+ },
+ {
+  key: 6,
+  name: "Joe Black",
+  age: 32,
+  address: "Sidney No. 1 Lake Park",
+  tags: ["cool", "teacher"],
+ },
+];
+
+export const colors = [
+ {
+  key: 1,
+  name: "John Brown",
+  colour: "primary",
+  flat: true,
+ },
+ {
+  key: 2,
+  name: "Jim Green",
+  colour: "secondary",
+  flat: true,
+ },
+ {
+  key: 3,
+  name: "Joe Black",
+  colour: "success",
+  flat: true,
+ },
+ {
+  key: 4,
+  name: "Kwame Opam",
+  colour: "warning",
+  flat: true,
+ },
+ {
+  key: 5,
+  name: "Julius Come",
+  colour: "error",
+  flat: true,
+ },
+ {
+  key: 6,
+  name: "Winnie Pooh",
+  colour: "gradient",
+  flat: true,
+ },
+];
