036af73bba4eeb299d8225b165b8a8a942862c4c
Sam-2019 <samtmartey@gmail.com>
2022-07-06 07:09:38 +0100

    jobs update

--- src/jobs/jobs.tsx
+++ src/jobs/jobs.tsx
@@ -1,21 +1,23 @@
 import { Grid } from "@nextui-org/react";
 import { Pagination } from "@nextui-org/react";
-import { CardItem } from "../components/card";
-import { userData } from "../utils/data";
+import { CardItem } from "../components/card copy";
 import Types from "./types";
 
-export default function JobList(dataSource: any) {
- console.log(dataSource);
- 
- return (
-  <Grid.Container justify="center" gap={2}>
-   <Types />
-   {userData.map((item) => (
-    <Grid xs={12} sm={4} key={item.key}>
-     <CardItem />
-    </Grid>
-   ))}
-   <Pagination total={10} initialPage={1} />
-  </Grid.Container>
- );
+export default function JobList({ dataSource }: any) {
+  return (
+    <div>
+      <Grid.Container justify="center" gap={2}>
+        <Types />
+
+        {dataSource.map((item: any) => (
+          <Grid xs={12} sm={4} key={item.id}>
+            <CardItem description={item.description} data={item} />
+          </Grid>
+        ))}
+      </Grid.Container>
+      <div>
+        <Pagination total={10} initialPage={1} />
+      </div>
+    </div>
+  );
 }
