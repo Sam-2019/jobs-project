84857770808fd6e4711d4fbd5546d0f0e34612c3
Sam-2019 <samtmartey@gmail.com>
2023-01-06 17:21:30 +0000

    updated data

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -1,83 +1,95 @@
 export const userData = [
- {
-  key: 1,
-  name: "John Brown",
-  age: 32,
-  address: "New York No. 1 Lake Park",
-  tags: ["nice", "developer"],
- },
- {
-  key: 2,
-  name: "Jim Green",
-  age: 42,
-  address: "London No. 1 Lake Park",
-  tags: ["loser"],
- },
- {
-  key: 3,
-  name: "Joe Black",
-  age: 32,
-  address: "Sidney No. 1 Lake Park",
-  tags: ["cool", "teacher"],
- },
- {
-  key: 4,
-  name: "John Brown",
-  age: 32,
-  address: "New York No. 1 Lake Park",
-  tags: ["nice", "developer"],
- },
- {
-  key: 5,
-  name: "Jim Green",
-  age: 42,
-  address: "London No. 1 Lake Park",
-  tags: ["loser"],
- },
- {
-  key: 6,
-  name: "Joe Black",
-  age: 32,
-  address: "Sidney No. 1 Lake Park",
-  tags: ["cool", "teacher"],
- },
+  {
+    id: 1,
+    description: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+    imgURL:
+      "https://pbs.twimg.com/profile_images/1231236382808563713/Q88JDBjw_400x400.jpg",
+  },
+  {
+    id: 2,
+    description: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+    imgURL:
+      "https://pbs.twimg.com/profile_images/1231236382808563713/Q88JDBjw_400x400.jpg",
+  },
+  {
+    id: 3,
+    description: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+    imgURL:
+      "https://pbs.twimg.com/profile_images/1231236382808563713/Q88JDBjw_400x400.jpg",
+  },
+  {
+    id: 4,
+    description: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+    imgURL:
+      "https://pbs.twimg.com/profile_images/1231236382808563713/Q88JDBjw_400x400.jpg",
+  },
+  {
+    id: 5,
+    description: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+    imgURL:
+      "https://pbs.twimg.com/profile_images/1231236382808563713/Q88JDBjw_400x400.jpg",
+  },
+  {
+    id: 6,
+    description: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+    imgURL:
+      "https://pbs.twimg.com/profile_images/1231236382808563713/Q88JDBjw_400x400.jpg",
+  },
 ];
 
 export const colors = [
- {
-  key: 1,
-  name: "John Brown",
-  colour: "primary",
-  flat: true,
- },
- {
-  key: 2,
-  name: "Jim Green",
-  colour: "secondary",
-  flat: true,
- },
- {
-  key: 3,
-  name: "Joe Black",
-  colour: "success",
-  flat: true,
- },
- {
-  key: 4,
-  name: "Kwame Opam",
-  colour: "warning",
-  flat: true,
- },
- {
-  key: 5,
-  name: "Julius Come",
-  colour: "error",
-  flat: true,
- },
- {
-  key: 6,
-  name: "Winnie Pooh",
-  colour: "gradient",
-  flat: true,
- },
+  {
+    id: 1,
+    description: "John Brown",
+    colour: "primary",
+    flat: true,
+  },
+  {
+    id: 2,
+    description: "Jim Green",
+    colour: "secondary",
+    flat: true,
+  },
+  {
+    id: 3,
+    description: "Joe Black",
+    colour: "success",
+    flat: true,
+  },
+  {
+    id: 4,
+    description: "Kwame Opam",
+    colour: "warning",
+    flat: true,
+  },
+  {
+    id: 5,
+    description: "Julius Come",
+    colour: "error",
+    flat: true,
+  },
+  {
+    id: 6,
+    description: "Winnie Pooh",
+    colour: "gradient",
+    flat: true,
+  },
 ];
