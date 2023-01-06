62fb0766bb139f80bfdbe33b5f7af6af16495119
Sam-2019 <samtmartey@gmail.com>
2023-01-06 17:08:19 +0000

    rename mode

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,3 +1,3 @@
-export const mode = import.meta.env.MODE;
+export const mode = import.meta.env.VITE_API_MODE;
 export const onlinehost = import.meta.env.VITE_API_URL;
 export const localhost = import.meta.env.VITE_API_URL_LOCAL;
