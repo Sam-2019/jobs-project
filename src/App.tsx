1ca0b5f9978efa6d47d0bca0500dc5d0d0836f93
Sam-2019 <samtmartey@gmail.com>
2023-01-07 03:42:15 +0000

    added theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { NextUIProvider } from "@nextui-org/react";
+import { NextUIProvider, createTheme } from "@nextui-org/react";
 import { Layout } from "./components/layout";
 import Root, { loader as rootLoader } from "./routes/root";
 import ErrorPage from "./pages/error";
 import Jobs, { loader as jobsLoader } from "./routes/jobs";
 import Home, { loader as homeLoader } from "./routes/home";
 import Twitter, { loader as twitterLoader } from "./routes/twitter";
+
+const theme = createTheme({
+  type: "light", // it could be "light" or "dark"
+  theme: {
+    colors: {
+      // brand colors
+      primaryLight: "$green200",
+      primaryLightHover: "$green300",
+      primaryLightActive: "$green400",
+      primaryLightContrast: "$green600",
+      primary: "#4ADE7B",
+      primaryBorder: "$green500",
+      primaryBorderHover: "$green600",
+      primarySolidHover: "$green700",
+      primarySolidContrast: "$white",
+      primaryShadow: "$green500",
+
+      gradient:
+        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
+      link: "#5E1DAD",
+
+      // you can also create your own color
+      myColor: "#ff4ecd",
+
+      // ...  more colors
+    },
+    space: {},
+    fonts: {},
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -35,7 +65,7 @@ const router = createBrowserRouter([
 
 export default function Index() {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={theme}>
       <Layout>
         <RouterProvider router={router} />;
       </Layout>
