576f4fb3dd0c2ee0607d727e403ccac51b4eeb72
Sam-2019 <samtmartey@gmail.com>
2023-01-07 03:32:26 +0000

    main update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Index from "./index";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { mode, onlinehost, localhost } from "./utils/config";
+import App from "./App";
 import "./index.css";
+
+const client = new ApolloClient({
+  uri: mode === "production" ? onlinehost : localhost,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Index />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
