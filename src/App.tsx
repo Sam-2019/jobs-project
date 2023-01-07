a2e564bca449bf14e8fc36aaeef4eb396ec7f084
Sam-2019 <samtmartey@gmail.com>
2023-01-07 03:32:09 +0000

    updated app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import { NextUIProvider, Container } from "@nextui-org/react";
-import Jobs from "./jobs";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
+import { Layout } from "./components/layout";
+import Root, { loader as rootLoader } from "./routes/root";
+import ErrorPage from "./pages/error";
+import Jobs, { loader as jobsLoader } from "./routes/jobs";
+import Home, { loader as homeLoader } from "./routes/home";
+import Twitter, { loader as twitterLoader } from "./routes/twitter";
 
-export default function App() {
- return (
-  <NextUIProvider>
-   <Container>
-    <Jobs />
-   </Container>
-  </NextUIProvider>
- );
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: homeLoader,
+      },
+      {
+        path: "/jobs",
+        element: <Jobs />,
+        loader: jobsLoader,
+      },
+      {
+        path: "/twitter",
+        element: <Twitter />,
+        loader: twitterLoader,
+      },
+    ],
+  },
+]);
+
+export default function Index() {
+  return (
+    <NextUIProvider>
+      <Layout>
+        <RouterProvider router={router} />;
+      </Layout>
+    </NextUIProvider>
+  );
 }
