44bd6f4163b25fdf860ef6e0c12556e216878f6e
Sam-2019 <samtmartey@gmail.com>
2023-01-08 18:05:09 +0000

    pagination update

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { Grid, Pagination } from "@nextui-org/react";
 import { CardItem } from "../components/card";
 
 export default function List({ dataSource, view = true }: any) {
+  const [state, setState] = React.useState(1);
+
   return (
     <div>
       <Grid.Container justify="center" gap={2}>
@@ -12,7 +15,11 @@ export default function List({ dataSource, view = true }: any) {
         ))}
       </Grid.Container>
       <Grid.Container justify="center">
-        <Pagination total={10} initialPage={1} />
+        <Pagination
+          total={10}
+          initialPage={state}
+          onChange={(info) => setState(info)}
+        />
       </Grid.Container>
     </div>
   );
