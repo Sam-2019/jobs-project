7e36ac2448891eb15198d8f10198a8937367090e
Sam-2019 <samtmartey@gmail.com>
2023-01-09 16:46:51 +0000

    updated router

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -7,7 +7,9 @@ import Home, { loader as homeLoader } from "../routes/home";
 import News, { loader as newsLoader } from "../routes/news";
 import Movies, { loader as moviesLoader } from "../routes/movies";
 import Twitter, { loader as twitterLoader } from "../routes/twitter";
+import { collapseItems } from "./data";
 
+console.log(collapseItems[0].path);
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -21,29 +23,29 @@ export const router = createBrowserRouter([
         loader: homeLoader,
       },
       {
-        path: "/jobs",
-        element: <Jobs />,
-        loader: jobsLoader,
-      },
-      {
-        path: "/twitter",
-        element: <Twitter />,
-        loader: twitterLoader,
-      },
-      {
-        path: "/news",
+        path: collapseItems[0].path,
         element: <News />,
         loader: newsLoader,
       },
       {
-        path: "/movies",
+        path: collapseItems[1].path,
+        element: <Art />,
+        loader: artLoader,
+      },
+      {
+        path: collapseItems[2].path,
         element: <Movies />,
         loader: moviesLoader,
       },
       {
-        path: "/art",
-        element: <Art />,
-        loader: artLoader,
+        path: collapseItems[3].path,
+        element: <Twitter />,
+        loader: twitterLoader,
+      },
+      {
+        path: collapseItems[4].path,
+        element: <Jobs />,
+        loader: jobsLoader,
       },
     ],
   },
