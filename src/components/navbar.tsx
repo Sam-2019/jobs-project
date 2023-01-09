763827f12c13bf1418db0640baa8dc70e59cbb71
Sam-2019 <samtmartey@gmail.com>
2023-01-09 16:46:41 +0000

    code update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,8 @@ import { useLocation } from "react-router-dom";
 import { AcmeLogo } from "./acmeLogo";
 import { collapseItems } from "../utils/data";
 
+collapseItems.shift();
+
 export default function Navigation() {
   let { pathname } = useLocation();
 
@@ -27,36 +29,15 @@ export default function Navigation() {
         hideIn="xs"
         variant="highlight-rounded"
       >
-        <Navbar.Link
-          isActive={setActive(collapseItems[0].path)}
-          href={collapseItems[0].path}
-        >
-          {collapseItems[0].name}
-        </Navbar.Link>
-        <Navbar.Link
-          isActive={setActive(collapseItems[1].path)}
-          href={collapseItems[1].path}
-        >
-          {collapseItems[1].name}
-        </Navbar.Link>
-        <Navbar.Link
-          isActive={setActive(collapseItems[2].path)}
-          href={collapseItems[2].path}
-        >
-          {collapseItems[2].name}
-        </Navbar.Link>
-        <Navbar.Link
-          isActive={setActive(collapseItems[3].path)}
-          href={collapseItems[3].path}
-        >
-          {collapseItems[3].name}
-        </Navbar.Link>
-        <Navbar.Link
-          isActive={setActive(collapseItems[4].path)}
-          href={collapseItems[4].path}
-        >
-          {collapseItems[4].name}
-        </Navbar.Link>
+        {collapseItems.map((item) => (
+          <Navbar.Link
+            key={item.id}
+            isActive={setActive(item.path)}
+            href={item.path}
+          >
+            {item.name}
+          </Navbar.Link>
+        ))}
       </Navbar.Content>
       <Navbar.Content
         css={{
